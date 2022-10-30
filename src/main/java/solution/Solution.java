4be4bb8d86d4f5771f86b3ad28f0a834425ed9e4
Ryan Zhu <ryan.zhu84@gmail.com>
2022-10-30 21:26:30 +1000

    Add the load record logic in the main solution.

--- src/main/java/solution/Solution.java
+++ src/main/java/solution/Solution.java
@@ -1,9 +1,29 @@
 package solution;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import model.TrafficData;
+
 public class Solution {
 	public static void main(String[] args) {
 
-        System.out.println("hello seek");
-
+//		ClassLoader classLoader = getClass().getClassLoader();
+//		File file = new File(classLoader.getResource("test_data.txt").getFile());
+		FileReader fileReader;
+		try {
+			fileReader = new FileReader("test_data.txt");
+			FileLoader fileLoader = new FileLoader();
+			
+			ArrayList<TrafficData> trafficDataList = fileLoader.loadRecordFile(fileReader);
+			
+			System.out.print(trafficDataList.size());
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			// TODO: error handler
+		}
+		
     }
 }
